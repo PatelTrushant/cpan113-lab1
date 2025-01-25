5a35a6f31727fcccd164eca4f291ecc1aba51605
PatelTrushant <trushantpatel96@hotmail.com>
2025-01-25 01:52:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+let studentName = "Trushant Patel";
+let studentAge = "28";
+let isEnrolled = true;
+
+console.log(studentName);
+console.log(studentAge);
+console.log(isEnrolled);
+
+let myName = "Smith"
+let myAge = "31";
+console.log(`my name is ${myName} my age is ${myAge}`)
+
+let distance = 150.5;
+let largeNumber = 900718825474099n;
+console.log(distance);
+console.log(largeNumber);
+
+let found = true;
+let lost = false;
+console.log(found);
+console.log(lost);
+found = false;
+lost = true;
+console.log(`now found is ${found} and lost is ${lost}`)
+
+let colors = ['Red', 'Green', 'Blue'];
+console.log(colors[0])
+colors.push('Yellow');
+console.log(colors);
+colors.pop();
+console.log(colors);
+
+let studentOne = { firstName: "Joe", lastName: "Smith", age: 90};
+console.log("studentOne:", studentOne);
+
+console.log(5 * 2);
+console.log(10 / 2);
+console.log(11 % 3);
+console.log( 5 + 3);
+console.log(10 - 2);
+
+let p = 5;
+let b = 10;
+console.log("10" == b);
+console.log("10" == 3);
+console.log(p != b);
+console.log(p < b);
+console.log(p > b);
+console.log(p <= b);
+console.log(p >= b);
+
+let c = true;
+let d = false;
+console.log(c && d);
+console.log(c || d);
+console.log(!c);
